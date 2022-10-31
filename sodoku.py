960b0152ae2ed87d2182285696d07d23ee7faff0
Eric Le <erichoangnle@gmail.com>
2022-10-31 13:16:11 +0700

    final version

--- sodoku.py
+++ sodoku.py
@@ -1,5 +1,6 @@
 import random
 
+
 def generate_initial_board():
     """
     Generate empty board with 3 diagonal boxes filled in with number from 1 to 9
@@ -22,62 +23,62 @@ def generate_initial_board():
     return board
 
 
-def row_safe(board, number, i, j):
+def is_safe(board, number, i, j):
     """
-    Check if number is already in row.
+    Check if number placement doesn't break game rules.
     """
+
+    # Check for row
     for k in range(9):
         if k != j:
             if number == board[i][k]:
                 return False
-    return True
 
-
-def column_safe(board, number, i, j):
-    """
-    Check if number is already in column.
-    """
+    # Check for column
     for k in range(9):
         if k != i:
             if number == board[k][j]:
                 return False
-    return True
 
-
-def box_safe(board, number, i, j):
-    """
-    Check if number is already in box.
-    """
+    # Check for box
     for x in range((i // 3) * 3, (i // 3) * 3 + 3):
         for y in range((j // 3) * 3, (j // 3) * 3 + 3):
             if x != i and y != j:
                 if number == board[x][y]:
                     return False
-    return True
 
-numbers = [_ for _ in range(1,10)]
-def fill_board(board):
-
-    for i in range(9):
-        for j in range(9):
-            if board[i][j] == 0:
-                for number in numbers:
-                    if row_safe(board, number, i, j) and column_safe(board, number, i, j) and box_safe(board, number, i, j):
-                        board[i][j] = number
-
-
-
-def filled(board):
-
-    for i in range(9):
-        for j in range(9):
-            if board[i][j] == 0:
-                return False
     return True
 
 
-board = generate_initial_board()
-fill_board(board)
-for row in board:
+def fill_board(board, i, j):
+    """
+    Create a solved sodoku puzzle.
+    """
+
+    if i == 8 and j == 9:
+        return True
+
+    if j == 9:
+        i += 1
+        j = 0
+
+    if board[i][j] != 0:
+        return fill_board(board, i, j + 1)
+
+    for number in range(1, 10):
+
+        if is_safe(board, number, i, j):
+
+            board[i][j] = number
+            if fill_board(board, i, j + 1):
+                return True
+
+        board[i][j] = 0
+
+    return False
+
+
+sodoku = generate_initial_board()
+fill_board(sodoku, 0, 0)
+for row in sodoku:
     print(row)
-print(f"Board filled: {filled(board)}")
